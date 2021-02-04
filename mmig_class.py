eba234dc50cb0b105e177e45939235f367f3fbe0
mwmuni <matthew.muller@uon.edu.au>
2021-02-04 18:06:46 +0800

    Moved the mesh interpolation functions into their own class for clarity.

---
+++ mmig_class.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.spatial import cKDTree as KDTree
+from typing import Tuple
+
+class mmig:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def process_kdtree_chunk(chunk: np.array, kdtree: KDTree):
+        """Run KDTree on a chunk of data
+        
+        Allows for splitting up the KDTree query to multiple cores, drastically improving speed"""
+        return kdtree.query(chunk)
+
+    @staticmethod
+    def interpolate(s_p: Tuple[float, float], interpolation: float) -> Tuple[float, float]:
+        """Moves s_p[0] towards s_p[1] by a factor of INTERPOLATION
+        
+        Used for np.apply_along_axis, this function gets applied vertically along the input array"""
+        s_p[0] = (1 - interpolation)*s_p[0] + interpolation*s_p[1]
+        # s_p[0] = s_p[0]+(-(s_p[0]-s_p[1])*interpolation)
+        return s_p
--- mmig_open3d.py
+++ mmig_open3d.py
@@ -1,30 +1,22 @@
 import numpy as np
-from scipy.spatial import cKDTree as KDTree
+from scipy.spatial import cKDTree as KDTree # cKDTree is a C implementation of KDTree, which is faster
 import open3d as o3d
 import sys
-from multiprocessing import Pool, cpu_count
-import functools
+from multiprocessing import Pool, cpu_count # Multiprocessing allows for running on multiple cores
+import functools # Used for the 'partial' function, which allows assigning keyword variables to multithreaded function mapping
 from time import time
-
-def process_kdtree_chunk(chunk, kdtree):
-    return kdtree.query(chunk)
-
-def interpolate(s_p):
-        s_p[0] = (1 - INTERPOLATION)*s_p[0] + INTERPOLATION*s_p[1]
-        return s_p
-
-INTERPOLATION = 0.5
+import mmig_class
 
 if __name__ == '__main__':
     start = time()
     if len(sys.argv) > 1:
-        _INTERPOLATION = float(sys.argv[1])
+        INTERPOLATION = float(sys.argv[1])
         _sphere = sys.argv[2]
         _ellipse = sys.argv[3]
     else:
         _sphere = "sphere.stl"
         _ellipse = "ellipse.stl"
-        _INTERPOLATION = 0.5
+        INTERPOLATION = 0.5
 
     print('loading meshes')
     _start = time()
@@ -44,14 +36,13 @@ if __name__ == '__main__':
     _end = time()
     print(f'finished loading meshes in {_end - _start}')
 
-    INTERPOLATION = _INTERPOLATION
-
     kdtree = KDTree(ellipse_points, leafsize=90)
+    mmig = mmig_class.mmig()
 
     print('about to query')
     _start = time()
     with Pool(cpu_count()) as p:
-        locations = p.map(functools.partial(process_kdtree_chunk, kdtree=kdtree), sphere_points, sphere_points.shape[0]//cpu_count())
+        locations = p.map(functools.partial(mmig.process_kdtree_chunk, kdtree=kdtree), sphere_points, sphere_points.shape[0]//cpu_count())
     _end = time()
     print(f'finised query in {_end - _start}')
 
@@ -59,12 +50,19 @@ if __name__ == '__main__':
 
     print('about to interpolate')
     _start = time()
-    inter = np.array(np.apply_along_axis(interpolate, 1, np.array([(s_p[1], ellipse_points[locations[s_p[0][0]]][s_p[0][1]]) for s_p in np.ndenumerate(sphere_points)], dtype=object)))
+    # lam = lambda x: (1 - INTERPOLATION)*x[0] + INTERPOLATION*x[1]
+    # lam = lambda x: x[0]+(-(x[0]-x[1])*INTERPOLATION)
+    # inter = np.array(np.apply_along_axis(lam, 1, np.array([(s_p[1], ellipse_points[locations[s_p[0][0]]][s_p[0][1]]) for s_p in np.ndenumerate(sphere_points)], dtype=object)))
+    # inter = np.array(np.apply_along_axis(lam, 1, np.array([(s_p[1], ellipse_points[locations[s_p[0][0]]][s_p[0][1]]) for s_p in np.ndenumerate(sphere_points)], dtype=object)))
+    # sphere_points[:] = inter.reshape((-1,3))
+    # For some reason, using lambda increases processing time by 0.7s, so I'll stick with partial
+    inter = np.array(np.apply_along_axis(functools.partial(mmig.interpolate, interpolation=INTERPOLATION), 1, np.array([(s_p[1], ellipse_points[locations[s_p[0][0]]][s_p[0][1]]) for s_p in np.ndenumerate(sphere_points)], dtype=object)))
     sphere_points[:] = inter[:, 0].reshape((-1,3))
     _end = time()
     print(f'finised interpolating in {_end - _start}')
 
-    sphere.compute_triangle_normals()
+    if _sphere.split('.')[-1].lower() == "stl":
+        sphere.compute_triangle_normals() # Required for STL
     o3d.io.write_triangle_mesh("interpolated."+_sphere.split('.')[-1], sphere)
 
     end = time()
